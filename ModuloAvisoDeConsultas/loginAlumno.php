d3939ee338d822a7b3f37502f5da8fc739fef30c
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2021-11-04 23:18:19 -0300

    Add files via upload

---
+++ ModuloAvisoDeConsultas/loginAlumno.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+    include('templates/header.php')
+?>
+
+
+<form>
+    <div class="titulo">
+        <h2>Inicio de sesión</h2>
+    </div>
+
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"checked>
+        <label class="form-check-label" for="inlineRadio1">Alumno</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" >
+        <label class="form-check-label" for="inlineRadio2">Docente</label>
+    </div>
+
+
+
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su email">
+    </div>
+
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Confirma que no eres un bot</label>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Ingresar</button>
+  </form>
+  
+<?php
+    include('templates/footer.php')
+?>
+
+</html>
