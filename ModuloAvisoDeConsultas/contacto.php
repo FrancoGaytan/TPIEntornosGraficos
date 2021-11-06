589c3624ea0b6666d5d165f17a1cb14f0c1ead6d
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2021-11-05 23:15:31 -0300

    Add files via upload

---
+++ ModuloAvisoDeConsultas/contacto.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+    include('templates/header.php')
+?>
+
+<link href="css/estilos.css" rel="stylesheet">
+
+<form>
+    <div class="titulo">
+        <h2>Contacto</h2>
+    </div>
+    
+    <div class="container">
+        <div class="ubicacionUTN" style="background-color:rgba(0, 0, 0, 0);">
+            <a style=float:right href="https://www.google.com/maps/place/Universidad+Tecnol%C3%B3gica+Nacional+%7C+Facultad+Regional+Rosario/@-32.9544347,-60.6437319,15z/data=!4m5!3m4!1s0x0:0x11f1d3d54f950dd0!8m2!3d-32.9544347!4d-60.6437319"><img class="img-responsive" src="img/utnUbicacion.jpg" data-bs-hover-animate="pulse" style="padding-top:10px;padding-bottom:10px;"></a>
+        </div>
+        <br>
+        <br>
+    </div>  
+
+</form>
+  
+<?php
+    include('templates/footer.php')
+?>
+
+</html>
---
+++ ModuloAvisoDeConsultas/listadoConsultas.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+    include('templates/header.php')
+?>
+
+<div class="container">
+    <form>
+        <div class="titulo">
+            <h2>Listado de Consultas</h2>
+        </div>
+
+        <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col">Consulta</th>
+            <th scope="col">Horario</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row"></th>
+                <td><button type="button" class="btn btn-outline-secondary">Inscribirse</button></td>
+                <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
+                <td>XX:XX hs</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td><button type="button" class="btn btn-outline-secondary">Inscribirse</button></td>
+                <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
+                <td>XX:XX hs</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td><button type="button" class="btn btn-outline-secondary">Inscribirse</button></td>
+                <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
+                <td>XX:XX hs</td>
+            </tr>
+        </tbody>
+        </table>
+    </form>
+
+</div>
+
+<?php
+    include('templates/footer.php')
+?>
+
+</html>
