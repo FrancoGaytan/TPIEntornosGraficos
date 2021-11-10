71ed7916fa60f4ee8d30ccc5fc461fadd392e2ef
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2021-11-10 16:33:08 -0300

    Update header.php

--- ModuloAvisoDeConsultas/templates/header.php
+++ ModuloAvisoDeConsultas/templates/header.php
@@ -5,42 +5,37 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <link href="css/estilos.css" rel="stylesheet">
-
+    <link href="css/estilos.css?v=2" rel="stylesheet">    <!-- ir variandolo -->
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
-
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-
     <title>Consultas UTN</title>
-
 </head>
-
 <body>
-
     <nav class="navbar navbar-expand-lg ">
         <div class="container">
             <a href="./base.php">
                 <img alt="Logo UTN" src="img/logo2.png" width="120" height="90">
              </a>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="./listadoConsultas.php" id="Horarios">Horarios</a>
+                        <a class="nav-link" href="#" id="Horarios">Horarios</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./regConsulta.php">Registro Consultas</a>
+                        <a class="nav-link">Registro Consultas</a>
                     </li>
                 </ul>
-                <div class="d-flex align-items-center">
-                <a class="nav-link" href="./inicioSesion.php" id="Ingresar">Ingresar</a>
-                <a class="nav-link" href="./regDocente.php" id="RegistroDocente">Registro Docente</a>
-                </div> 
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./loginAlumno.php"id="IngresoAlumnos">Ingreso Alumno</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./regDocente.php" id="RegistroDocente">Registro Docente</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
