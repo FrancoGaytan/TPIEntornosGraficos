4a25dac0a5d8c292e145a097dc067f1a546574c0
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-02 19:56:07 -0300

    Update validarDocente.php

--- ModuloAvisoDeConsultas/utils/validarDocente.php
+++ ModuloAvisoDeConsultas/utils/validarDocente.php
@@ -2,8 +2,7 @@
 $email=$_POST['email'];
 $contraseña=$_POST['contrasenia'];
 
-include_once('destroySession.php');
-include_once('isSessionStarted.php');
+session_destroy();
 
 include('db.php');
 
@@ -13,15 +12,20 @@ $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
 $fila=mysqli_fetch_array($resultado);
 
 if(mysqli_num_rows($resultado) > 0){
+    if (!session_id())
+        session_start();
     $_SESSION['email']=$email;
+    $_SESSION['legajo']=$fila['legajo'];
+    $_SESSION['nombre']=$fila['nombre'];
+    $_SESSION['apellido']=$fila['apellido'];
     $_SESSION['esDocente']=true;
-    $_SESSION['esAlumno']=false;
-    header("location: ../regConsulta.php");
+    //$_SESSION['esAlumno']=false; Agrego esto?
+    header("location:../regConsulta.php");
 }else{
     echo '<div class="alert alert-danger" role="alert">
     Error en la autenticación
     </div>';
-    include("../loginDocente.php");
+    include("loginDocente.php");
 }
 
 mysqli_free_result($resultado);
