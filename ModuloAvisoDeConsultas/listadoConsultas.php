46fa19466ed296cdde3c50d197ddeae24a7748da
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-02 19:56:55 -0300

    Update listadoConsultas.php

--- ModuloAvisoDeConsultas/listadoConsultas.php
+++ ModuloAvisoDeConsultas/listadoConsultas.php
@@ -26,11 +26,28 @@
         <div class="titulo">
             <h4>Bienvenido <?php echo $_SESSION['nombre']; echo ' '; echo $_SESSION['apellido'] ?>!</h4>
         </div>
+       
+        <?php
+
+        /*$fecha_consulta=$_POST['fecha_consulta'];
+
+        $query= "SELECT * FROM consultas WHERE fecha_consulta >= current_date()";
+        $result = mysqli_query($conexion, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while($fila = mysqli_fetch_assoc($result)){
+            */
+        ?>
+
         <select class="form-select" aria-label="Default select example">
             <option selected>Buscar Materia</option>
-            <option value="1">Entornos Graficos</option>
-            <option value="2">Analisis Matematico</option>
-            <option value="3">Fisica 2</option>
+            <option value="1">Fisica II</option>
+            <option value="2">Diseño de Sistemas</option>
+            <option value="3">Comunicaciones</option>
+            <option value="4">Redes</option>
+            <option value="5">Investigacion Operativa</option>
+            <option value="6">Simulacion</option>
+            <option value="7">Entornos Graficos</option>
         </select>
         <table class="table table-striped">
         <thead>
@@ -45,7 +62,7 @@
             <tr>
                 <th scope="row"></th>
                 <td><button type="button" class="btn btn-outline-secondary">Inscribirse</button></td>
-                <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
+                <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX <?php/* echo $fila["fecha_consulta"]*/?></p></td>
                 <td>XX:XX hs</td>
             </tr>
             <tr>
@@ -63,10 +80,11 @@
         </tbody>
         </table>
     </form>
-
+          
 </div>
 
 <?php
+/*}}*/
     include('templates/footer.php')
 ?>
 
