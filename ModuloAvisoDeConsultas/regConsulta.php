ccd1197351485d50e38732d3c4feadc65f522120
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-03 17:18:12 -0300

    Update regConsulta.php

--- ModuloAvisoDeConsultas/regConsulta.php
+++ ModuloAvisoDeConsultas/regConsulta.php
@@ -11,41 +11,45 @@
 
 <link href="css/estilos.css" rel="stylesheet">
 
+
+
 <div class="contenido">
-  <form class="formulario">
+  <form action="utils/registrarConsulta.php" method="post" class="formulario">
       <div class="titulo">
           <h2>Registro de Nueva Consulta</h2>
       </div>
 
-      <select class="form-select" aria-label="Default select example">
+      <select name="idMateria" class="form-select" aria-label="Default select example">
           <option selected>Materia</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-      </select>
-      
-      <select class="form-select" aria-label="Default select example">
-          <option selected>Dia de consulta</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option value="11">Fisica II</option>
+          <option value="18">Diseño de Sistemas</option>
+          <option value="19">Comunicaciones</option>
+          <option value="25">Redes</option>
+          <option value="27">Investigacion Operativa</option>
+          <option value="28">Simulacion</option>
+          <option value="44">Entornos Graficos</option>
       </select>
 
-
+      <div class="form-group">
+        <label for="exampleInputHorarios1">Dia de Consulta</label>
+        <input name="fechaConsulta" type="date" class="form-control" id="diaConsulta" aria-describedby="emailHelp" placeholder="Ingrese el horario de consulta">
+      </div>
 
       <div class="form-group">
         <label for="exampleInputEmail1">Horario de Consulta</label>
-        <input type="time" class="form-control" id="horarioConsulta" aria-describedby="emailHelp" placeholder="Ingrese el horario de consulta">
+        <input name="horaConsulta" type="time" class="form-control" id="horarioConsulta" aria-describedby="emailHelp" placeholder="Ingrese el horario de consulta">
       </div>
 
       <div class="form-group">
         <label for="exampleInputEmail1">Cupo maximo de consulta</label>
-        <input type="number" class="form-control" id="cupoConsulta" aria-describedby="emailHelp" placeholder="Ingrese el cupo de la consulta">
+        <input name="cupoConsulta" type="number" class="form-control" id="cupoConsulta" aria-describedby="emailHelp" placeholder="Ingrese el cupo de la consulta">
       </div>
 
       <button type="submit" class="btn btn-primary">Confirmar</button>
     </form>
 </div>
+
+?>
 
 <?php
     include('templates/footer.php')
