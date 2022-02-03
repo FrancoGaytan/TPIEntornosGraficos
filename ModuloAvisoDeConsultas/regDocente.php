32021ad90a2eb76b13103da3570adbe07e8cf53a
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-03 17:17:21 -0300

    Update regDocente.php

--- ModuloAvisoDeConsultas/regDocente.php
+++ ModuloAvisoDeConsultas/regDocente.php
@@ -6,7 +6,7 @@
 ?>
 
 <div class="contenido">
-  <form class="formulario">
+  <form action= "utils/registrarDocente.php" method="post" class="formulario">
       
       <div class="titulo">
           <h2>Registro Docente</h2>
@@ -14,27 +14,27 @@
 
       <div class="form-group">
           <label for="exampleInputNombre">Nombre</label>
-          <input type="text" class="form-control" id="exampleInputNombre" aria-describedby="emailHelp" placeholder="Ingrese su nombre">
+          <input name="nombreDocente" type="text" class="form-control" id="nombreDocente" aria-describedby="emailHelp" placeholder="Ingrese su nombre">
           <small id="nombreHelp" class="form-text text-muted">No compartiremos su nombre con nadie más.</small>
         </div>
         <div class="form-group">
           <label for="exampleInputApellido">Apellido</label>
-          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su apellido">
+          <input name="apellidoDocente" type="text" class="form-control" id="apellidoDocente" aria-describedby="emailHelp" placeholder="Ingrese su apellido">
           <small id="apellidoHelp" class="form-text text-muted">No compartiremos su apellido con nadie más.</small>
         </div>
         <div class="form-group">
           <label for="exampleInputEmail1">Legajo</label>
-          <input type="number" class="form-control" id="exampleInputLegajo" aria-describedby="emailHelp" placeholder="Ingrese su legajo">
+          <input name="legajoDocente" type="number" class="form-control" id="legajoDocente" aria-describedby="emailHelp" placeholder="Ingrese su legajo">
           <small id="legajoHelp" class="form-text text-muted">No compartiremos su id con nadie más.</small>
         </div>
       <div class="form-group">
         <label for="exampleInputEmail1">Email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su email">
+        <input name="emailDocente" type="email" class="form-control" id="emailDocente" aria-describedby="emailHelp" placeholder="Ingrese su email">
         <small id="emailHelp" class="form-text text-muted">No compartiremos su email con nadie más.</small>
       </div>
       <div class="form-group">
         <label for="exampleInputContraseña">Contraseña</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese su contraseña">
+        <input name="contraseñaDocente" type="password" class="form-control" id="contraseñaDocente" placeholder="Ingrese su contraseña">
       </div>
       <div class="form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
