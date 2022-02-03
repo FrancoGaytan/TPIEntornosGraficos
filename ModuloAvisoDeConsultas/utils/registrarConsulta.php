63255dedbff310741cc5ade33aabc17d32f2d050
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-03 17:15:45 -0300

    Add files via upload

---
+++ ModuloAvisoDeConsultas/utils/registrarConsulta.php
@@ -0,0 +1,20 @@
+<?php
+$idMateria=$_POST['idMateria'];
+$fechaConsulta=$_POST['fechaConsulta'];
+$horaConsulta=$_POST['horaConsulta'];
+$cupoConsulta=$_POST['cupoConsulta'];
+
+$_SESSION['legajo']=$legajoDocente;
+
+include('db.php');
+
+$consulta="INSERT INTO consultas (fecha_consulta, hora_consulta, cupo, id_profesor, id_materia) VALUES ('$fechaConsulta', '$horaConsulta', '$cupoConsulta', '$legajoDocente', '$idMateria')";
+$resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
+
+//aca falta hacer el if por si la conexion falla o no se registra correctamente
+    
+header("location:../misConsultasDoc.php");
+
+mysqli_close($conexion);
+
+?>
