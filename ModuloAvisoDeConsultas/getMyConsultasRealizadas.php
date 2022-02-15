6413abbea5b63673cd199e498a3da931146c49d8
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-14 22:02:29 -0300

    Add files via upload

---
+++ ModuloAvisoDeConsultas/getMyConsultasRealizadas.php
@@ -0,0 +1,44 @@
+<?php
+
+include('db.php');
+
+$legajoDoc = $_SESSION['legajo'];
+$consulta="SELECT * FROM consultas WHERE fecha_hora < curdate() and id_profesor = {$legajoDoc};";
+$resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
+
+if ($resultado->num_rows > 0) {
+    while($row = $resultado->fetch_assoc()) {
+        $formatoInput = 'Y-m-d H:i:s';
+        $fechaHora = DateTime::createFromFormat($formatoInput, $row["fecha_hora"]);
+        $hora = $fechaHora -> format('H:i');
+        $fecha = $fechaHora -> format('d-m-Y');
+
+
+        $legProf = $row["id_profesor"];
+        $consulta2="SELECT nombre, apellido FROM profesores WHERE legajo = {$legProf};";
+        $resultado2=mysqli_query($conexion,$consulta2) or die (mysqli_error($conexion));
+        $row2 = $resultado2->fetch_assoc();
+
+        echo "
+            <tr>
+                <th scope='row'></th>
+                <td>  
+                </td>
+                <td><p>Profesor: " . $row2["nombre"], ' ', $row2["apellido"]. "</p><p>Cupos no ocupados: " . $row["cupo"] . "</p><b>Día de consulta Realizada: " . $fecha . "</b></td>
+                <td><b>" . $hora . "</b></td>
+            </tr>
+        ";
+    }
+}
+else {
+    echo "
+        <tr>
+            <th scope='row'></th>
+            <td></td>
+            <td><b>No ha realizado consultas</b></td>
+            <td></td>
+        </tr>
+    ";
+}
+
+?>
---
+++ ModuloAvisoDeConsultas/historialConsultasDoc.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+    include('./restringidoDocente.php')
+?>
+
+<?php
+    include('templates/header.php')
+?>
+
+<div class="container">
+    <section class="main row">
+        <article class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <br>
+            <img src="img/perfilVacio.jpg" alt="..." class="img-thumbnail">
+            <br>
+            <br>
+            <h3><?php echo $_SESSION['nombre']; echo ' '; echo $_SESSION['apellido'] ?></h3>
+            <h4>Legajo: <?php echo $_SESSION['legajo'] ?></h4>
+            <p>Este es el parrafo de prueba de la pagina de consultas del docente donde estos pueden agregar informacion adicional para su perfil </p>
+            <td><button type="button" class="btn btn-outline-secondary" onclick="window.location.href = 'misConsultasDoc.php'">Regresar a mis consultas</button></td>
+
+        </article>
+
+        <aside class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+        <div class="container">
+
+                <div class="titulo">
+                    <h2>Mis Consultas Realizadas</h2>
+                </div>
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col">Consulta</th>
+                        <th scope="col">Horario</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        include('getMyConsultasRealizadas.php')
+                        ?>
+                    </tbody>
+                </table>
+
+            </div>
+        </aside>
+  
+    </section>
+</div>
+
+<?php
+    include('templates/footer.php')
+?>
+
+</html>
