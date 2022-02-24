a6f30f0f46f6ac5dbe5b8a23ff6a9cf20eb860be
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-02-23 21:14:08 -0300

    Update perfilAlumno.php

--- ModuloAvisoDeConsultas/perfilAlumno.php
+++ ModuloAvisoDeConsultas/perfilAlumno.php
@@ -25,12 +25,12 @@
 
         <aside class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
         <div class="container">
-                <form>
-                    <div class="titulo">
-                        <h2>Mis Proximas Consultas</h2>
-                    </div>
 
-                    <table class="table table-striped">
+                <div class="titulo">
+                    <h2>Mis Proximas Consultas</h2>
+                </div>
+
+                <table class="table table-striped">
                     <thead>
                         <tr>
                         <th scope="col"></th>
@@ -40,35 +40,17 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row"></th>
-                            <td><button type="button" class="btn btn-outline-secondary">Eliminar</button></td>
-                            <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
-                            <td>XX:XX hs</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"></th>
-                            <td><button type="button" class="btn btn-outline-secondary">Eliminar</button></td>
-                            <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
-                            <td>XX:XX hs</td>
-                        </tr>
-                        <tr>
-                            <th scope="row"></th>
-                            <td><button type="button" class="btn btn-outline-secondary">Eliminar</button></td>
-                            <td><p>Profesor: XX</p><p>Cupos Disponibles: XX</p><p>Día de consulta: XX/XX/XXXX</p></td>
-                            <td>XX:XX hs</td>
-                        </tr>
+                        <?php
+                        include('getMyConsultasAlumno.php')
+                        ?>
                     </tbody>
-                    </table>
-                </form>
+                </table>
 
             </div>
         </aside>
   
     </section>
 </div>
-
-
 
 <?php
     include('templates/footer.php')
