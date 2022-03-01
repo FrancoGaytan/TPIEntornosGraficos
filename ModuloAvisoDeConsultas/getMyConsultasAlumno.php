77635d5e916a5d36f51629e97627123d075b9905
Federico Navós <fedenavos@gmail.com>
2022-03-01 16:52:13 -0300

    Cambios en getMyConsultasAlumno  y cancelarInscripcion

--- ModuloAvisoDeConsultas/getMyConsultasAlumno.php
+++ ModuloAvisoDeConsultas/getMyConsultasAlumno.php
@@ -5,48 +5,45 @@ include('./restringidoAlumno.php');
 include('db.php');
 
 $legajoAlu = $_SESSION['legajo'];
-$consultasAlumno="SELECT * FROM alumno_consulta WHERE legajo_alumno = {$legajoAlu} and cancelada = 0;";
+$consultasAlumno="SELECT * FROM alumno_consulta AS ac 
+                    JOIN consultas AS c ON c.id = ac.id_consulta
+                    WHERE ac.legajo_alumno = $legajoAlu 
+                        and ac.cancelada = 0
+                        and c.estado = 1
+                        and c.fecha_hora > curdate();";
 $resultado=mysqli_query($conexion,$consultasAlumno) or die (mysqli_error($conexion));
 
 if ($resultado->num_rows > 0) {
     while($row = $resultado->fetch_assoc()) {
 
-        $idConsulta = $row["id_consulta"];
-        $consulta2="SELECT * FROM consultas WHERE estado = 1 and fecha_hora > curdate() and id = {$idConsulta};";
-        $resultado2=mysqli_query($conexion,$consulta2) or die (mysqli_error($conexion));
-        while ($row2 = $resultado2->fetch_assoc()){
+        $formatoInput = 'Y-m-d H:i:s';
+        $fechaHora = DateTime::createFromFormat($formatoInput, $row["fecha_hora"]);
+        $hora = $fechaHora -> format('H:i');
+        $fecha = $fechaHora -> format('d-m-Y');
 
-            $formatoInput = 'Y-m-d H:i:s';
-            $fechaHora = DateTime::createFromFormat($formatoInput, $row2["fecha_hora"]);
-            $hora = $fechaHora -> format('H:i');
-            $fecha = $fechaHora -> format('d-m-Y');
-
-            $legProf = $row2["id_profesor"];
-            $consulta3="SELECT nombre, apellido FROM profesores WHERE legajo = {$legProf};";
-            $resultado3=mysqli_query($conexion,$consulta3) or die (mysqli_error($conexion));
-            while($row3 = $resultado3->fetch_assoc()) {
-                
-                $idMateria =  $row2["id_materia"];
-                $consulta4="SELECT nombre_materia FROM materias WHERE id = {$idMateria};";
-                $resultado4=mysqli_query($conexion,$consulta4) or die (mysqli_error($conexion));
-                while($row4 = $resultado4->fetch_assoc()) {
-                    echo "
-                        <tr>
-                            <th scope='row'></th>
-                            <td>
-                                <a style='color: red' onClick=\"javascript: return confirm('Estas seguro que se quiere desinscribir de la consulta ". $idConsulta . "?');\" href='cancelarConsulta.php?legAlumno =".$legajoAlu." id=".$idConsulta."'>Cancelar</a>
-                                
-                                <form action='./cancelarConsulta.php' method='post'>
-                                    <input type='hidden' name='id' value='" . $idConsulta . "'/><br>
-                                    <input type='hidden' name='legAlumno' value='" . $legajoAlu . "'/><br>
-                                    <input type='submit' value='Cancelar' />
-                                </form>
-                            </td>
-                            <td><p><b>Nombre Materia: " . $row4["nombre_materia"]. "</b> <p>Profesor: " . $row3["nombre"], ' ', $row3["apellido"]. "</p><p>Cupos Disponibles: " . $row2["cupo"] . "</p>Día de consulta: " . $fecha . "</b></td>
-                            <td><b>" . $hora . "</b></td>
-                        </tr>
-                    ";
-                }
+        $legProf = $row["id_profesor"];
+        $consulta3="SELECT nombre, apellido FROM profesores WHERE legajo = {$legProf};";
+        $resultado3=mysqli_query($conexion,$consulta3) or die (mysqli_error($conexion));
+        while($row3 = $resultado3->fetch_assoc()) {
+            
+            $idMateria =  $row["id_materia"];
+            $consulta4="SELECT nombre_materia FROM materias WHERE id = {$idMateria};";
+            $resultado4=mysqli_query($conexion,$consulta4) or die (mysqli_error($conexion));
+            while($row4 = $resultado4->fetch_assoc()) {
+                echo "
+                    <tr>
+                        <th scope='row'></th>
+                        <td>
+                            <form action='./cancelarInscripcion.php' method='post'>
+                                <input type='hidden' name='id' value='" . $row["id_consulta"] . "'/><br>
+                                <input type='hidden' name='legAlumno' value='" . $legajoAlu . "'/><br>
+                                <input type='submit' value='Cancelar' />
+                            </form>
+                        </td>
+                        <td><p><b>Nombre Materia: " . $row4["nombre_materia"]. "</b> <p>Profesor: " . $row3["nombre"], ' ', $row3["apellido"]. "</p><p>Cupos Disponibles: " . $row["cupo"] . "</p>Día de consulta: " . $fecha . "</b></td>
+                        <td><b>" . $hora . "</b></td>
+                    </tr>
+                ";
             }
         }
     }
