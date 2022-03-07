f8b45071885d84b43538111dca6bc1cf6cdbc382
Federico Navós <fedenavos@gmail.com>
2022-03-07 12:54:21 -0300

    css loginAlumno fixed

--- ModuloAvisoDeConsultas/loginAlumno.php
+++ ModuloAvisoDeConsultas/loginAlumno.php
@@ -7,18 +7,20 @@
 
 <link href="css/estilos.css" rel="stylesheet">
     
-<form action="./validarAlumno.php" method="post">
-    <div class="titulo">
-        <h2>Ingreso alumno</h2>
-    </div>
-
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su email">
-    </div>
-
-    <button type="submit" class="btn btn-primary">Ingresar</button>
-  </form>
+<div class="contenido margined">
+  <form action="./validarAlumno.php" method="post" class="formulario">
+      <div class="titulo">
+          <h2>Ingreso alumno</h2>
+      </div>
+  
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email</label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su email">
+      </div>
+  
+      <button type="submit" class="btn btn-primary margined">Ingresar</button>
+    </form>
+</div>
   
 <?php
     include('templates/footer.php')
