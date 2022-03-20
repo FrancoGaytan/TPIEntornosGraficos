db0796529d22fbd76885a336a002f52d9040d8f0
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 14:46:30 -0300

    Update cancelarInscripcion.php

--- ModuloAvisoDeConsultas/cancelarInscripcion.php
+++ ModuloAvisoDeConsultas/cancelarInscripcion.php
@@ -3,7 +3,7 @@
 $id = $_POST['id'];
 $legAlumno = $_POST['legAlumno'];
 
-include($_SERVER["DOCUMENT_ROOT"] .'db.php');
+include('db.php');
 
 $consulta="DELETE FROM alumno_consulta WHERE id_consulta={$id} and legajo_alumno={$legAlumno};";
 $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
@@ -11,12 +11,11 @@ $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
 $consulta2="UPDATE consultas SET cupo = cupo + 1 WHERE id = {$id};";
 $resultado2=mysqli_query($conexion,$consulta2) or die (mysqli_error($conexion));
 
-echo "<div class='alert alert-success' role='alert'>
-La consulta fue cancelada correctamente
-</div>";
-include($_SERVER["DOCUMENT_ROOT"] ."./perfilAlumno.php");
-
-mysqli_free_result($resultado);
+if(is_object($resultado))
+    mysqli_free_result($resultado);
 mysqli_close($conexion);
+
+header("Location: listadoConsultas.php?msg=".urlencode("La inscripción a consulta fue eliminada correctamente"));
+exit();
 
 ?>
