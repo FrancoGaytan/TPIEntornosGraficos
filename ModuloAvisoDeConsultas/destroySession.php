29d550d51e4320d62ac95a24e98b7a415df6cbc7
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 14:49:24 -0300

    Update destroySession.php

--- ModuloAvisoDeConsultas/destroySession.php
+++ ModuloAvisoDeConsultas/destroySession.php
@@ -1,10 +1,13 @@
 <?php
 
+$_SESSION = array();
+
 if (session_id()) {
     session_unset();
+    unset($_SESSION);
     session_destroy();
     session_write_close();
-    session_regenerate_id(true);
+    // session_regenerate_id(true);
     // setcookie(session_name(),'',0,'/');
 }
 
