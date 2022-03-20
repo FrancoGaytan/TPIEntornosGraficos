5cf49869a5ce2d2d4a4526901a03aa5d0603629c
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 14:59:48 -0300

    Update reprogramarConsulta.php

--- ModuloAvisoDeConsultas/reprogramarConsulta.php
+++ ModuloAvisoDeConsultas/reprogramarConsulta.php
@@ -1,12 +1,12 @@
 <?php
-    include($_SERVER["DOCUMENT_ROOT"] .'./restringidoDocente.php')
+    include('./restringidoDocente.php')
 ?>
 
 <!doctype html>
 <html lang="en">
 
 <?php
-    include($_SERVER["DOCUMENT_ROOT"] .'templates/header.php')
+    include('templates/header.php')
 ?>
 
 <link href="css/estilos.css" rel="stylesheet">
@@ -19,12 +19,12 @@
 
       <div class="form-group">
         <label for="fechaConsulta">Dia de Consulta</label>
-        <input name="fechaConsulta" type="date" class="form-control" id="diaConsulta" aria-describedby="emailHelp" placeholder="Ingrese el día de consulta">
+        <input name="fechaConsulta" type="date" class="form-control" id="diaConsulta" aria-describedby="emailHelp" placeholder="Ingrese el día de consulta" required>
       </div>
 
       <div class="form-group">
         <label for="horaConsulta">Horario de Consulta</label>
-        <input name="horaConsulta" type="time" class="form-control" id="horaConsulta" aria-describedby="emailHelp" placeholder="Ingrese el horario de consulta">
+        <input name="horaConsulta" type="time" class="form-control" id="horaConsulta" aria-describedby="emailHelp" placeholder="Ingrese el horario de consulta" required>
       </div>
 
       <input type='hidden' name='id' value='<?php echo $_POST['id'] ?>'/><br>
@@ -36,7 +36,7 @@
 ?>
 
 <?php
-    include($_SERVER["DOCUMENT_ROOT"] .'templates/footer.php')
+    include('templates/footer.php')
 ?>
 
 </html>
