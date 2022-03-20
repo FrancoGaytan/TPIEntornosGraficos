ccf80ef26ce1ef59ce415944d4068d1776f56e3e
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 15:01:40 -0300

    Update validarDocente.php

--- ModuloAvisoDeConsultas/validarDocente.php
+++ ModuloAvisoDeConsultas/validarDocente.php
@@ -1,16 +1,18 @@
 <?php
+include_once('isSessionStarted.php');
 $email=$_POST['email'];
 $contraseña=$_POST['contrasenia'];
 
-include_once($_SERVER["DOCUMENT_ROOT"] .'destroySession.php');
-include_once($_SERVER["DOCUMENT_ROOT"] .'isSessionStarted.php');
+//include_once('destroySession.php');
+$_SESSION = array();
 
-include($_SERVER["DOCUMENT_ROOT"] .'db.php');
+include('db.php');
 
 $consulta="SELECT * FROM profesores WHERE email='$email' and contrasenia='$contraseña'";
 $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
 
 $fila=mysqli_fetch_array($resultado);
+
 
 if(mysqli_num_rows($resultado) > 0){
     $_SESSION['email']=$email;
@@ -19,12 +21,14 @@ if(mysqli_num_rows($resultado) > 0){
     $_SESSION['apellido']=$fila['apellido'];
     $_SESSION['esDocente']=true;
     $_SESSION['esAlumno']=false;
+    
     header("location:./regConsulta.php");
+    exit();
 }else{
     echo '<div class="alert alert-danger" role="alert">
     Error en la autenticación
     </div>';
-    include($_SERVER["DOCUMENT_ROOT"] ."loginDocente.php");
+    include("loginDocente.php");
 }
 
 mysqli_free_result($resultado);
