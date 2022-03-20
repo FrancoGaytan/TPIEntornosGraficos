51b4809f0cc71db5095420bbc5121ef8c2550a39
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 14:53:39 -0300

    Update inscribirAlumno.php

--- ModuloAvisoDeConsultas/inscribirAlumno.php
+++ ModuloAvisoDeConsultas/inscribirAlumno.php
@@ -1,11 +1,11 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] .'isSessionStarted.php');
+include_once('isSessionStarted.php');
 
 $idConsulta=$_GET['id'];
 $legajo=$_SESSION['legajo'];
 
-include($_SERVER["DOCUMENT_ROOT"] .'db.php');
+include('db.php');
 
 mysqli_init();
 $consulta="INSERT INTO alumno_consulta (legajo_alumno, id_consulta) VALUES ('$legajo', '$idConsulta')";
@@ -15,7 +15,8 @@ if (mysqli_errno($conexion) == 1062) {
     echo "<div class='alert alert-danger' role='alert'>
     Ya estas registrado a esta consulta.
     </div>";
-    include($_SERVER["DOCUMENT_ROOT"] ."./listadoConsultas.php");
+    include("./listadoConsultas.php");
+    exit();
 }
 
 $consulta2="UPDATE consultas SET cupo = cupo - 1 WHERE id = $idConsulta;";
@@ -24,7 +25,7 @@ $resultado2=mysqli_query($conexion,$consulta2) or die (mysqli_error($conexion));
 echo "<div class='alert alert-success' role='alert'>
 La inscripción fue registrada correctamente.
 </div>";
-include($_SERVER["DOCUMENT_ROOT"] ."./listadoConsultas.php");
+include("./listadoConsultas.php");
 
 mysqli_free_result($resultado);
 mysqli_free_result($resultado2);
