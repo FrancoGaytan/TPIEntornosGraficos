3d26d75a717ae74f0d5d75a987c2ac86db43ea15
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 14:58:17 -0300

    Update registrarConsulta.php

--- ModuloAvisoDeConsultas/registrarConsulta.php
+++ ModuloAvisoDeConsultas/registrarConsulta.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] .'isSessionStarted.php');
+include_once('isSessionStarted.php');
 
 $idMateria=$_POST['idMateria'];
 $fechaConsulta=$_POST['fechaConsulta'];
@@ -13,17 +13,16 @@ $formatoOutput = 'Y-m-d H:i:s';
 $fechaHora = DateTime::createFromFormat($formatoInput, "$fechaConsulta $horaConsulta");
 $fechaString = $fechaHora -> format($formatoOutput);
 
-include($_SERVER["DOCUMENT_ROOT"] .'db.php');
+include('db.php');
 
 $consulta="INSERT INTO consultas (fecha_hora, cupo, id_profesor, id_materia) VALUES ('$fechaString', '$cupoConsulta', '$legajo', '$idMateria')";
 $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
 
-echo "<div class='alert alert-success' role='alert'>
-La consulta fue agregada correctamente
-</div>";
-include($_SERVER["DOCUMENT_ROOT"] ."./regConsulta.php");
-
-mysqli_free_result($resultado);
+if(is_object($resultado))
+    mysqli_free_result($resultado);
 mysqli_close($conexion);
+
+header("Location: regConsulta.php?msg=".urlencode("La consulta fue agregada correctamente"));
+exit();
 
 ?>
