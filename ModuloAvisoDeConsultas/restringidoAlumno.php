701a89bbc83a16709e9ae1b053f37b7b11414e8d
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 15:00:16 -0300

    Update restringidoAlumno.php

--- ModuloAvisoDeConsultas/restringidoAlumno.php
+++ ModuloAvisoDeConsultas/restringidoAlumno.php
@@ -1,12 +1,12 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] .'isSessionStarted.php');
+include_once('isSessionStarted.php');
 
 if ((!isset($_SESSION['esAlumno'])) or (!$_SESSION['esAlumno'])){ 
     echo '<div class="alert alert-danger" role="alert">
     Solo los Alumnos tienen acceso a esta página
     </div>';
-    include($_SERVER["DOCUMENT_ROOT"] ."inicioSesion.php");
+    include("index.php");
     die();
 }
 
