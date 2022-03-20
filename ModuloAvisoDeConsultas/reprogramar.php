ab0097aaee27647a2ebc06337ff87422a34be316
FrancoGaytan <80915496+FrancoGaytan@users.noreply.github.com>
2022-03-20 14:59:13 -0300

    Update reprogramar.php

--- ModuloAvisoDeConsultas/reprogramar.php
+++ ModuloAvisoDeConsultas/reprogramar.php
@@ -4,25 +4,22 @@ $id = $_POST['id'];
 $fecha = $_POST['fechaConsulta'];
 $hora = $_POST['horaConsulta'];
 
-echo $fecha;
-echo $hora;
-
 $formatoInput = 'Y-m-d H:i';
 $fechaHora = DateTime::createFromFormat($formatoInput, $fecha . ' ' . $hora);
 $formatoOutput = 'Y-m-d H:i:s';
 $fechaString = $fechaHora -> format($formatoOutput);
 
-include($_SERVER["DOCUMENT_ROOT"] .'db.php');
+include('db.php');
 
 $consulta="UPDATE consultas SET fecha_hora = '$fechaString' WHERE id = $id;";
 $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
 
-echo "<div class='alert alert-success' role='alert'>
-La consulta fue modificada correctamente
-</div>";
-include($_SERVER["DOCUMENT_ROOT"] ."./misConsultasDoc.php");
+if(is_object($resultado))
+    mysqli_free_result($resultado);
 
-mysqli_free_result($resultado);
 mysqli_close($conexion);
+
+header("Location: misConsultasDoc.php?msg=".urlencode("La consulta fue modificada correctamente"));
+exit();
 
 ?>
