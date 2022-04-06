91299480e38d9d3aa01fbf73274bc5767bbe7d7b
Federico Navós <fedenavos@gmail.com>
2022-04-06 18:09:00 -0300

    PHPMailer añadido + mail contacto funcionando

--- ModuloAvisoDeConsultas/enviarFormulario.php
+++ ModuloAvisoDeConsultas/enviarFormulario.php
@@ -1,13 +1,35 @@
-<?php 
+<?php
+ 
+//librerias
+use PHPMailer\PHPMailer\PHPMailer;
+
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
 $nombre=$_POST['nombre'];
 $email=$_POST['email'];
 $telefono=$_POST['telefono'];
 $asunto=$_POST['asunto'];
 $mensaje=$_POST['mensaje'];
-
-$to = "fedenavos@gmail.com,gaytanfranco@gmail.com";
-$subject = "Mensaje de Consultas UTN";
-$message = "
+ 
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+$mail->IsSMTP();
+ 
+//Configuracion servidor mail
+$mail->From = "consultas.utn.entornos@gmail.com"; //remitente
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls'; //seguridad
+$mail->Host = "smtp.gmail.com"; // servidor smtp
+$mail->Port = 587; //puerto
+$mail->Username ='consultas.utn.entornos@gmail.com'; //nombre usuario
+$mail->Password = 'consultas123'; //contraseña
+ 
+//Agregar destinatario
+$mail->AddAddress("fedenavos@gmail.com");
+$mail->AddAddress("francogaytan@gmail.com");
+$mail->Subject = "Mensaje en Consultas UTN";
+$mensaje = "
 Llego una consulta en su sitio web con los siguientes datos: \n
 Nombre: $nombre \n
 Email: $email \n
@@ -15,8 +37,15 @@ Telefono: $telefono \n
 Asunto: $asunto \n
 Mensaje: $mensaje 
 ";
+$mail->Body = $mensaje;
  
-mail($to, $subject, $message);
-
-echo 'Email enviado';
-?>
+//Avisar si fue enviado o no y dirigir al index
+if ($mail->Send()) {
+    echo'<script type="text/javascript">
+           alert("Enviado Correctamente");
+        </script>';
+} else {
+    echo'<script type="text/javascript">
+           alert("NO ENVIADO, intentar de nuevo");
+        </script>';
+}
