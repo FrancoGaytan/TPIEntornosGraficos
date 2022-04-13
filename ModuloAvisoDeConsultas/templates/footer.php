fbeab7e63b74616457e094d373c33a8cb1b31754
Federico Navós <fedenavos@gmail.com>
2022-04-13 19:24:38 -0300

    error de los tests arreglado

--- ModuloAvisoDeConsultas/templates/footer.php
+++ ModuloAvisoDeConsultas/templates/footer.php
@@ -10,11 +10,8 @@
                 <!-- Grid column -->
                 <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                     <!-- Content -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        <i class="fas fa-gem me-3"></i>
-                    </h6>
                     <div class="container-fluid" style="background-color:rgba(0, 0, 0, 0);">
-                        <a href="https://www.frro.utn.edu.ar/"><img class="img-responsive" src="img/logo.png" data-bs-hover-animate="pulse" style="padding-top:10px;padding-bottom:10px;"></a>
+                        <a href="https://www.frro.utn.edu.ar/"><img alt="logo-footer" class="img-responsive" src="img/logo.png" data-bs-hover-animate="pulse" style="padding-top:10px;padding-bottom:10px;"></a>
                     </div>
                 </div>
                 
@@ -29,9 +26,7 @@
                     <p>
                         <a href="./perfilAlumno.php" class="text-reset">Mi Perfil</a>
                     </p>
-
                 </div>
-
                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                     
                     <h6 class="text-uppercase fw-bold mb-4">
@@ -43,29 +38,22 @@
                     <p>
                         <a href="./misConsultasDoc.php" class="text-reset">Mis Consultas</a>
                     </p>
-
                 </div>
-
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         Facultad
                     </h6>
-
                     <p>
                         <a href="./contacto.php" class="text-reset">Contacto</a>
                     </p>
-
                     <p>
-                        <a href="https://www.frro.utn.edu.ar/" target=”_blank” class="text-reset">Acerca de nosotros</a>
+                        <a href="https://www.frro.utn.edu.ar/contenido.php?cont=343&subc=16" target=”_blank” class="text-reset">Acerca de nosotros</a>
                     </p>
                 </div>
-
             </div>
-
         </div>
     </section>
-
     <!-- Copyright -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
         © 2021 Copyright:
