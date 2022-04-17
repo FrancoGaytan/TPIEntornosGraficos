43dfd01fed659d5cdc28d25621c477b130953737
Federico Navós <fedenavos@gmail.com>
2022-04-17 11:41:30 -0300

    ajuste de consulta bloqueada

--- ModuloAvisoDeConsultas/getMyConsultas.php
+++ ModuloAvisoDeConsultas/getMyConsultas.php
@@ -3,7 +3,7 @@
 include('db.php');
 
 $legajoDoc = $_SESSION['legajo'];
-$consulta="SELECT * FROM consultas WHERE estado = 1 and fecha_hora > curdate() and id_profesor = {$legajoDoc};";
+$consulta="SELECT * FROM consultas WHERE estado != 2 and fecha_hora > curdate() and id_profesor = {$legajoDoc};";
 $resultado=mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
 
 if ($resultado->num_rows > 0) {
@@ -23,11 +23,18 @@ if ($resultado->num_rows > 0) {
             <tr>
                 <th scope='row'></th>
                 <td>
-                    <a style='color: red' onClick=\"javascript: return confirm('Estas seguro de eliminar la consulta?');\" href='blockConsulta.php?id=".$row['id']."'>Bloquear</a>
-                    <form action='./reprogramarConsulta.php' method='post'>
-                        <input type='hidden' name='id' value='" . $row["id"] . "'/><br>
-                        <input type='submit' value='Reprogramar' />
-                    </form>
+            ";
+        if($row["estado"] == 1) {
+            echo "<a style='color: blue' onClick=\"javascript: return confirm('Estas seguro de bloquear la consulta?');\" href='blockConsulta.php?id=".$row['id']."'>Bloquear</a>";
+        } else if ($row["estado"] == 0) {
+            echo "<a style='color: blue' onClick=\"javascript: return confirm('Estas seguro de desbloquear la consulta?');\" href='unblockConsulta.php?id=".$row['id']."'>Desbloquear</a>";
+        }
+        echo "
+                <a style='color: red' onClick=\"javascript: return confirm('Estas seguro de eliminar la consulta?');\" href='deleteConsulta.php?id=".$row['id']."'>Eliminar</a>
+                <form action='./reprogramarConsulta.php' method='post'>
+                    <input type='hidden' name='id' value='" . $row["id"] . "'/><br>
+                    <input type='submit' value='Reprogramar' />
+                </form>
                 </td>
                 <td><p>Profesor: " . $row2["nombre"], ' ', $row2["apellido"]. "</p><p>Cupos Disponibles: " . $row["cupo"] . "</p><b>Día de consulta: " . $fecha . "</b></td>
                 <td><b>" . $hora . "</b></td>
---
+++ ModuloAvisoDeConsultas/unblockConsulta.php
@@ -0,0 +1,18 @@
+<?php
+
+$id = $_GET['id'];
+
+include($_SERVER["DOCUMENT_ROOT"] .'/db.php');
+
+$consulta="UPDATE consultas SET estado = 1 WHERE id = $id;";
+$resultado = mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
+
+if(is_object($resultado))
+    mysqli_free_result($resultado);
+
+mysqli_close($conexion);
+
+header("Location: misConsultasDoc.php?msg=".urlencode("La consulta fue desbloqueada correctamente"));
+exit();
+
+?>
